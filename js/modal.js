db5c88a12022515fc25c4e190ae49b17a5e81914
Margarita555 <a_rita@ukr.net>
2021-06-25 20:04:34 +0300

    +

--- js/modal.js
+++ js/modal.js
@@ -7,8 +7,10 @@
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  
 
   function toggleModal() {
     refs.modal.classList.toggle("backdrop--is-hidden");
+    document.body.classList.toggle("modal-open");
   }
 })();
